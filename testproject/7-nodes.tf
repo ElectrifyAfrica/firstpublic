a0c83082eb12b9a05a52c7fd7cda32a54d3d63e7
Daniel Aikhonmu <odaikhonmu@gmail.com>
2024-11-17 05:41:10 -0500

    updated commit

--- testproject/7-nodes.tf
+++ testproject/7-nodes.tf
@@ -49,9 +49,9 @@ resource "aws_eks_node_group" "private-nodes" {
   instance_types = ["t2.medium"]
 
   scaling_config {
-    desired_size = 1
+    desired_size = 2
     max_size     = 10
-    min_size     = 0
+    min_size     = 1
   }
 
   update_config {
